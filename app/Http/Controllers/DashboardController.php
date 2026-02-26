0280c68b043e4de5414ff4cf90a690c9da8b413e
Miguel Angel Cornielly H <mcornielly@gmail.com>
2026-02-26 12:08:07 -0400

    Add index method to DashboardController and render view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Display the dashboard page.
+     *
+     * This route is protected by auth/verified middleware in web.php.
+     */
+    public function index()
+    {
+        return Inertia::render('Dashboard');
+    }
 }
